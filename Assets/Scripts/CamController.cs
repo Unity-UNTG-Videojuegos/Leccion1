c62ffba2cadafb4942a528cb80e4b9a9ca8fe1d4
Gabriela-Rosas <rocagaby2001@gmail.com>
2022-09-30 00:11:57 -0500

    Leccion1

---
+++ Assets/Scripts/CamController.cs
@@ -0,0 +1,25 @@
+/*
+Rosas Castillo Gabriela
+Creación de videojuegos
+Lección1_Challenge 
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamController : MonoBehaviour
+{
+    public GameObject tanque;
+    public Vector3 upCam = new Vector3(0,10,-10);
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = tanque.transform.position + upCam;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+/*
+Rosas Castillo Gabriela
+Creación de videojuegos
+Lección1_Challenge 
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocidad = 20;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float forwardInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        //We'll move the vehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * velocidad * forwardInput); 
+        transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+    }
+}
